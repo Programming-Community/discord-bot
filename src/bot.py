5a401c0a81d786e068627b2e32eda59797ecbbd4
Dillon Davis <07734willy@users.noreply.github.com>
2020-08-05 12:02:04 -0500

    refactored permissions into decorator, added delete/reaction ack, added rich embed responses

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,9 @@
 from client import Client
 from tables import ROLE_JOIN_MSGS
 from permissions import *
+from discord import Embed, Color
+from discord.errors import NotFound
+from contextlib import suppress
 import re
 import os
 
@@ -9,54 +12,79 @@ COMMAND_PREFIX = ["/"]
 
 client = Client(command_prefix=COMMAND_PREFIX)
 
-@client.command(name='say', pass_context=True)
-async def say(ctx):
+COLOR_GREEN = 0x447744
+COLOR_RED = 0xAA4444
+
+async def rich_send(channel, text, title=None, color=None):
+	color = Color(color) if color else Color.blue() 
+	title = title or ""
+	embed = Embed(title=title, description=text, color=color)
+	return await channel.send('\n', embed=embed)
+
+
+def command(name, roles=None, channels=None, categories=None, delete_parent=False):
+	def decorator(func):
+		async def f(ctx, *args, **kwargs):
+			channel = ctx.channel
+			if not await check_scope_perms(ctx.message, channels, categories):
+				return
+			if not await check_role_perms(ctx.message, roles):
+				await rich_send(channel, "You do not have permissions to run this command", title='Permission Denied', color=COLOR_RED)
+				return
+
+			try:
+				await func(ctx, *args, **kwargs)
+			except Exception as e:
+				await rich_send(channel, str(e), title='Internal Error', color=COLOR_RED)
+				await ctx.message.add_reaction(u"\u274C")
+			else:
+				with suppress(NotFound):
+					await ctx.message.add_reaction(u"\u2705")
+					if delete_parent:
+						await ctx.message.delete(delay=3)
+		return client.command(name=name)(f)
+	return decorator
+					
+@command(name='say')
+async def say(ctx, *args):
 	text = ctx.message.content.split(maxsplit=1)[1]
-	await ctx.channel.send(text)
+	await rich_send(ctx.channel, text, color=COLOR_GREEN)
 
-@client.command(name='rolejoin')
-async def rolejoin(ctx):
-	if not await check_perms(ctx.message, roles=[ROLE_ADMIN], channels=[CHANNEL_ASSIGN_ROLES]):
-		return
-
-	await ctx.channel.send("React to any of the following messages to join the corresponding role")
+@command(name='rolejoin', roles=[ROLE_ADMIN], channels=[CHANNEL_ASSIGN_ROLES])
+async def rolejoin(ctx, *args):
+	await rich_send(ctx.channel, "React to any of the following messages to join the corresponding role")
 
 	for role in ctx.message.role_mentions:
-		msg = await ctx.channel.send(f" - {role.mention}")
+		msg = await rich_send(ctx.channel, f" - {role.mention}")
 		ROLE_JOIN_MSGS[msg.id] = role.id
 		await msg.add_reaction(u"\U0001F44D")
 
-@client.command(name='deletemsg')
+@command(name='deletemsg', roles=[ROLE_ADMIN])
 async def deletemsg(ctx, count):
-	if not await check_perms(ctx.message, roles=[ROLE_ADMIN]):
-		return
-
 	async for msg in ctx.channel.history(limit=int(count) + 1):
 		await msg.delete()
 
-@client.command(name='suggest')
-async def suggest(ctx):
-	if not await check_perms(ctx.message, channels=[CHANNEL_SUGGESTIONS]):
-		return
-
+@command(name='suggest', channels=[CHANNEL_SUGGESTIONS])
+async def suggest(ctx, *args):
+	length_limit = 30
 	suggestion = ctx.message.content.split(maxsplit=1)[1]
 	title = re.sub(r"[^\w\-]", "", re.sub(r" +", "-", suggestion)) # alphanumeric + "_" and "-"
 
-	if len(title) > 20:
-		await ctx.channel.send("Project title must be no more than 20 characters")
+	if len(title) > length_limit:
+		await rich_send(ctx.channel, f"Project title must be no more than {length_limit} characters")
 		return
 
 	category = client.get_channel(CATEGORY_PROJECTS)
 	project_channel = await category.create_text_channel(title)
 
-	await project_channel.send(
+	await rich_send(project_channel,
 f"This channel is for discussion of the {project_channel.name} project. Here you can hash out requirements, scope, etc., \
 before diving into the project itself and writing any code. \n\
 Refer to the channels under `Help` and `Templates` for info on setting up your team \
 using any of our provided templates. Of course, they are entirely optional, and exist solely \
 to help you team get started.")
 	
-	msg = await ctx.channel.send(f"React to this message to vote for \"{title}\". Checkout {project_channel.mention} for discussion")
+	msg = await rich_send(ctx.channel, f"React to this message to vote for \"{title}\". Checkout {project_channel.mention} for discussion")
 	await msg.add_reaction(u"\U0001F44D")
 
 client.run(TOKEN)
--- src/permissions.py
+++ src/permissions.py
@@ -3,21 +3,23 @@ ROLE_ADMIN = 738803963041939567
 
 CHANNEL_ASSIGN_ROLES = 738819172846272553
 CHANNEL_SUGGESTIONS = 739621224598208556
+CHANNEL_TESTING = 739578107115339796
 
 CATEGORY_PROJECTS = 738890618956415026
 
 
-async def check_perms(msg, roles=[], channels=[]): # todo, add categories
-	if channels and msg.channel.id not in channels:
-		# silently fail
-		return False
+async def check_scope_perms(msg, channels, categories):
+	category_id = getattr(getattr(msg, 'category', None), 'id', None)
 
+	return not ((channels or categories) and
+		(not channels or msg.channel.id not in channels) and
+		(not categories or category_id not in categories))
+
+async def check_role_perms(msg, roles):
 	if not roles:
 		return True
 
 	for role in msg.author.roles:
 		if role.id in roles:
 			return True
-	
-	await msg.channel.send("You do not have permission for this command")
 	return False
